49dd2afe550e5ab2761e6b9ec6030ab0fac21e46
zhengwei.zhou <anbangzhiguo@163.com>
2018-03-23 14:09:36 +0800

    up

--- src/main/webapp/WEB-INF/js/modules/jslist/jslist.js
+++ src/main/webapp/WEB-INF/js/modules/jslist/jslist.js
@@ -4,7 +4,34 @@ define(function (require,exports,module) {
     var VueComponent = Vue.extend({
         template: sTpl,
         methods:{
+            tirggerFile:function (e) {
+                var self = this;
+                var files = e.target.files;
+                var form = new FormData(),
+                    url = '/uploadJs', //服务器上传地址
+                    file = files[0];
+                form.append('file', file);
 
+                var xhr = new XMLHttpRequest();
+                xhr.open("post", url, true);
+                xhr.upload.addEventListener("progress", function(result) {
+                    if (result.lengthComputable) {
+                        var percent = (result.loaded / result.total * 100).toFixed(2);
+                    }
+                }, false);
+
+                xhr.addEventListener("readystatechange", function() {
+                    var result = xhr;
+                    if (result.status != 200) { //error
+                        console.log('上传失败', result.status, result.statusText, result.response);
+                    }
+                    else if (result.readyState == 4) { //finished
+                        console.log('上传成功', result);
+                        alert('上传成功');
+                    }
+                });
+                xhr.send(form); //开始上传
+            }
         },
         data:function(){
             return {
--- src/main/webapp/WEB-INF/js/modules/pluglist/pluglist.js
+++ src/main/webapp/WEB-INF/js/modules/pluglist/pluglist.js
@@ -14,6 +14,36 @@ define(function (require,exports,module) {
                         alert(res);
                     }
                 });
+            },
+            tirggerFile:function (e) {
+                var self = this;
+                var files = e.target.files;
+                var form = new FormData(),
+                    url = '/uploadPlug', //服务器上传地址
+                    file = files[0];
+                form.append('file', file);
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("post", url, true);
+                xhr.upload.addEventListener("progress", function(result) {
+                    if (result.lengthComputable) {
+                        var percent = (result.loaded / result.total * 100).toFixed(2);
+                    }
+                }, false);
+
+                xhr.addEventListener("readystatechange", function() {
+                    var result = xhr;
+                    if (result.status != 200) { //error
+                        console.log('上传失败', result.status, result.statusText, result.response);
+                    }
+                    else if (result.readyState == 4) { //finished
+                        console.log('上传成功', result);
+                        $.get("/getPlugs", function(res){
+                            self.pluglist = res;
+                        });
+                    }
+                });
+                xhr.send(form); //开始上传
             }
         },
         data:function(){
